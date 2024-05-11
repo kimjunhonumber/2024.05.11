168992bdae35de1f2addad444105f8108f1de350
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-05-11 14:02:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 import os
 
 
+def print_messages():
+
+    if "messages" in st.session_state and len(st.session_state["messages"]) > 0 :
+        for chat_message in st.session_state["messages"]:
+            st.chat_message(chat_message.role).write(chat_message.content)
+
+
+class StreamingHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text="", **kwargs) -> None:
+        self.container = container
+        self.text = initial_text
+   
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
 st.set_page_config(page_title="ChatGpt", page_icon="🚀")
 st.title("🚀ChatGPT")
 
